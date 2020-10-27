4200534eca2c79e8a481da00810765089a3afd33
KiryanovKD <kiryanovKD@yandex.ru>
2020-10-27 22:10:10 +0300

    change name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="imagegen",
+    name="ImageGen",
     version="0.0.1",
     author="kiryanovkd",
     description="imageget utils",
